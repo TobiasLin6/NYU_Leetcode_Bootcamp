f6ae0fc7f399b70bddae8408e2d8656f8bbed03f
tobiaslin <tobiaslin6@gmail.com>
2025-10-20 13:34:49 -0400

    Week 3 submissions

--- Week 3/Set Matrix Zeros.py
+++ Week 3/Set Matrix Zeros.py
@@ -7,14 +7,12 @@ class Solution:
         zero_rows = set()
         zero_cols = set()
         
-        # First pass: identify rows and columns to zero
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
                     zero_rows.add(i)
                     zero_cols.add(j)
         
-        # Second pass: set zeros
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if i in zero_rows or j in zero_cols:
